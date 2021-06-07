1206ab5d050ac5bd3468e31790fe3b4f40212441
Valeri Karpov <val@karpov.io>
2021-06-07 10:41:35 -0400

    fix(worker): make worker compile correctly with changes from #8

--- worker/main.go
+++ worker/main.go
@@ -20,8 +20,11 @@ func main() {
 	// This worker hosts both Worker and Activity functions
 	w := worker.New(c, "CART_TASK_QUEUE", worker.Options{})
 
-	w.RegisterActivity(app.CreateStripeCharge)
-	w.RegisterActivity(app.SendAbandonedCartEmail)
+	var a *app.Activities
+
+	w.RegisterActivity(a.CreateStripeCharge)
+	w.RegisterActivity(a.SendAbandonedCartEmail)
+
 	w.RegisterWorkflow(app.CartWorkflow)
 	// Start listening to the Task Queue
 	err = w.Run(worker.InterruptCh())
